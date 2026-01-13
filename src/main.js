701a9b0703a157338b3db2213a06e322b7f7a0c8
Gabriel Havas <gabehavas@gmail.com>
2026-01-13 14:51:05 -0500

    Remove primevue

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import PrimeVue from "primevue/config";
-import Drawer from "primevue/drawer";
-import Button from "primevue/button";
-import Aura from "@primeuix/themes/aura";
 
 const app = createApp(App);
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-  },
-});
-app.component("Button", Button);
-app.component("Drawer", Drawer);
 app.mount("#app");
